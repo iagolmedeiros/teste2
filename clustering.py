c61faec3accb3f311a48f847da7e19ce16213c61
iagolmedeiros <iagolmedeiros@gmail.com>
2020-04-08 11:54:26 -0400

    Mean Shift adicionado (tem como melhorar)
    Atualmente tá adicionado o Meanshift e registrando os resultados dele, de forma preguiçosa.
    Tem como melhorar, passando só o nome do algoritmo que quero.

--- clustering.py
+++ clustering.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.cluster import MeanShift
 
 with open("positions.txt") as f:
     lines = [i.split() for i in f.readlines()]
@@ -11,6 +12,13 @@ print(kmeans.labels_)
 cluster_centers = np.array(kmeans.cluster_centers_)
 with open("centers.txt", "w") as c:
     for center in kmeans.cluster_centers_:
+        coords = f"{center[0]} {center[1]} {center[2]}\n"
+        c.write(coords)
+
+ms = MeanShift().fit(lines)
+cluster_centers = np.array(ms.cluster_centers_)
+with open("centers.txt", "w") as c:
+    for center in ms.cluster_centers_:
         coords = f"{center[0]} {center[1]} {center[2]}\n"
         c.write(coords)
 
